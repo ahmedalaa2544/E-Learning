6febcc2fcc9d4447acea4757ac27fb96af726508
ahmedalaa2544 <ahmedalaa2544@yahoo.com>
2023-12-11 09:09:07 +0200

    updates

--- src/utils/asyncHandling.js
+++ src/utils/asyncHandling.js
@@ -1,7 +1,7 @@
 export const asyncHandler = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((error) => {
-      // console.log(error)
+      console.log(error);
       return next(error);
     });
   };
