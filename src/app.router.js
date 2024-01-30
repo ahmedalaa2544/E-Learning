1e3c348da60e98960d4f596ba1be0ca0a6a31583
ahmedalaa2544 <ahmedalaa2544@yahoo.com>
2024-01-30 11:17:29 +0200

    app.router updates

--- src/app.router.js
+++ src/app.router.js
@@ -1,8 +1,13 @@
-import connectDB from "../DB/connection.js";
+mport connectDB from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
 import userRouter from "./modules/user/user.router.js";
 import uploadRouter from "./modules/upload/upload.router.js";
+import workshopRouter from "./modules/workshop/workshop.router.js";
+// import roomRouter from "./modules/room/room.router.js"
 import courseRouter from "./modules/course/course.router.js";
+import categRouter from "./modules/category/categ.router.js";
+import SubCategRouter from "./modules/subCategory/subCateg.router.js";
+import cartRouter from "./modules/cart/cart.router.js";
 import { globalErrorHandler } from "./utils/asyncHandling.js";
 import cors from "cors";
 
@@ -16,6 +21,11 @@ const appRouter = (app, express) => {
   app.use("/user", userRouter);
   app.use("/upload", uploadRouter);
   app.use("/course", courseRouter);
+  app.use("/workshop", workshopRouter);
+  // app.use("/room", roomRouter);
+  app.use("/category", categRouter);
+  app.use("/subCategory", SubCategRouter);
+  app.use("/cart", cartRouter);
 
   app.all("*", (req, res) => {
     return res.status(404).json({ message: "invalid Path" });
@@ -25,4 +35,4 @@ const appRouter = (app, express) => {
   app.use(globalErrorHandler);
 };
 
-export default appRouter;
+export default appRouter;
