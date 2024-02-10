6f047230c5f7e39be78df97d2748367cd4c04479
Ahmed-Elashmony <ahmedelashmony59@gmail.com>
2024-02-10 18:40:49 +0200

    fix status code

--- src/middleware/authntication.middleware.js
+++ src/middleware/authntication.middleware.js
@@ -8,31 +8,31 @@ const isAuth = asyncHandler(async (req, res, next) => {
   let { token } = req.headers;
 
   if (!token?.startsWith(process.env.BEARER_TOKEN)) {
-    return next(new Error("required valid token"), { cause: 400 });
+    return next(new Error("required valid token"), { cause: 401 });
   }
   // check payload
   token = token.split(process.env.BEARER_TOKEN)[1];
   const decoded = jwt.verify(token, process.env.TOKEN_SIGNTURE);
   if (!decoded?.id) {
-    return next(new Error("inVaild Payload", { cause: 404 }));
+    return next(new Error("inVaild Payload", { cause: 401 }));
   }
   req.userId = decoded.id;
   const user = await userModel.findById(decoded.id);
   // check Online and deleted accounts
   if (!user?.isOnline) {
-    return next(new Error("LogIn First", { cause: 400 }));
+    return next(new Error("LogIn First", { cause: 401 }));
   }
   if (user.isDeleted) {
     return next(
       new Error("UR ACC Is Deleted,LogIn Before 30th to recover it", {
-        cause: 400,
+        cause: 401,
       })
     );
   }
   // check token in DB
   const tokenDB = await tokenModel.findOne({ token, valid: true });
   if (!tokenDB) {
-    return next(new Error("Token Not Vaild", { cause: 404 }));
+    return next(new Error("Token Not Vaild", { cause: 401 }));
   }
   // pass user
   req.user = user;
