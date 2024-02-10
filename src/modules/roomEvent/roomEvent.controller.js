66583fd6d10fc47b6524f35ab154776064467def
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2024-02-10 07:56:30 +0200

    test room event

--- src/modules/roomEvent/roomEvent.controller.js
+++ src/modules/roomEvent/roomEvent.controller.js
@@ -12,7 +12,24 @@ export const room_started = asyncHandler(async (req, res, next) => {
   // event is a WebhookEvent object
   const event = receiver.receive(req.body, req.get("Authorization"));
 
-  // update room statur
+  // test webhook
+  await roomModel.create({
+    roomName: "eslam",
+  });
+  console.log(event);
+});
+
+export const room_finished = asyncHandler(async (req, res, next) => {
+  // create receiver
+  const receiver = new WebhookReceiver(
+    process.env.LIVEKIT_API_KEY,
+    process.env.LIVEKIT_SECRET_KEY
+  );
+
+  // event is a WebhookEvent object
+  const event = receiver.receive(req.body, req.get("Authorization"));
+  
+  // test webhook
   await roomModel.create({
     roomName: "eslam",
   });
