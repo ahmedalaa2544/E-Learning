f725c2d5ee32801be1d779b448bf529d70d2aa44
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2024-02-11 21:37:05 +0200

    fix roomEvent bugs

--- src/modules/roomEvent/roomEvent.router.js
+++ src/modules/roomEvent/roomEvent.router.js
@@ -20,31 +20,31 @@ router.post(
 );
 
 // participant_joined Event
-router.post(
-  "/participant_joined",
-  express.raw({ type: "application/webhook+json" }),
-  roomEventController.participant_joined
-);
+// router.post(
+//   "/participant_joined",
+//   express.raw({ type: "application/webhook+json" }),
+//   roomEventController.participant_joined
+// );
 
 // participant_left Event
-router.post(
-  "/participant_left",
-  express.raw({ type: "application/webhook+json" }),
-  roomEventController.participant_left
-);
+// router.post(
+//   "/participant_left",
+//   express.raw({ type: "application/webhook+json" }),
+//   roomEventController.participant_left
+// );
 
 // track_published Event
-router.post(
-  "/track_published",
-  express.raw({ type: "application/webhook+json" }),
-  roomEventController.track_published
-);
+// router.post(
+//   "/track_published",
+//   express.raw({ type: "application/webhook+json" }),
+//   roomEventController.track_published
+// );
 
 // track_unpublished Event
-router.post(
-  "/track_unpublished",
-  express.raw({ type: "application/webhook+json" }),
-  roomEventController.track_unpublished
-);
+// router.post(
+//   "/track_unpublished",
+//   express.raw({ type: "application/webhook+json" }),
+//   roomEventController.track_unpublished
+// );
 
 export default router;
