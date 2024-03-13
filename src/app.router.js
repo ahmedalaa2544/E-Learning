c8fb3a2616db507b66d4ca9a5e2f1c3a00f054e6
ahmedalaa2544 <ahmedalaa2544@yahoo.com>
2024-03-13 16:58:39 +0200

    corse updates

--- src/app.router.js
+++ src/app.router.js
@@ -22,18 +22,18 @@ const appRouter = (app, express) => {
   // Cors // ////////
   app.use(cors({ origin: true, credentials: true }));
 
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.setHeader("Access-Control-Allow-Origin", "*");
+  //   res.setHeader(
+  //     "Access-Control-Allow-Methods",
+  //     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  //   );
+  //   res.setHeader(
+  //     "Access-Control-Allow-Headers",
+  //     "Content-Type, Authorization"
+  //   );
+  //   next();
+  // });
 
   app.use((req, res, next) => {
     if (
