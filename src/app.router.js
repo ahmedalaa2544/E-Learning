ed39b56f1f8e43681d30b628592c44d67fe46859
ahmedalaa2544 <ahmedalaa2544@yahoo.com>
2024-03-13 16:51:54 +0200

    cors updates

--- src/app.router.js
+++ src/app.router.js
@@ -21,6 +21,18 @@ import useragent from "express-useragent";
 const appRouter = (app, express) => {
   // Cors // ////////
   app.use(cors({}));
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  });
 
   app.use((req, res, next) => {
     if (
