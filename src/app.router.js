b94b63149b845b02e5e8470b8fd7f955ade58489
Ahmed-Elashmony <ahmedelashmony59@gmail.com>
2024-04-16 16:05:47 +0200

    notifications after order

--- src/app.router.js
+++ src/app.router.js
@@ -13,7 +13,7 @@ import couponRouter from "./modules/coupon/coupon.router.js";
 import analyticsRouter from "./modules/analytics/analytics.router.js";
 import quizRouter from "./modules/quiz/quiz.router.js";
 import chatRouter from "./modules/chat/chat.router.js";
-import chatGroupRouter from "./modules/chatGroup/chat.router.js";
+import chatGroupRouter from "./modules/chatGroup/chat.Grouprouter.js";
 import recommendationRouter from "./modules/recommendation/recommendation.router.js";
 import { globalErrorHandler } from "./utils/asyncHandling.js";
 import cors from "cors";
--- src/modules/chatGroup/chat.Grouprouter.js
+++ src/modules/chatGroup/chat.Grouprouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
-import * as chatController from "./chat.controller.js";
+import * as chatController from "./chatGroup.controller.js";
 import isAuth from "../../middleware/authntication.middleware.js";
 
 router.get("/", isAuth, chatController.Chats);
@@ -9,6 +9,6 @@ router.post("/send", isAuth, chatController.sendMsg);
 
 router.post("/", isAuth, chatController.createChat);
 
-router.get("/:destId", isAuth, chatController.getChat);
+router.get("/:chatId", isAuth, chatController.getChat);
 
 export default router;
